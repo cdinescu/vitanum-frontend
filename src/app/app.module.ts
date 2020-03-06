a1c0634249fae2a1b49095ed54c1a6da30b8cca5
Cristina Dinescu <crina91@gmail.com>
2020-03-06 19:10:51 +0200

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DiaryComponent } from './components/diary/diary.component';
+import { TabbedMenuComponent } from './components/tabbed-menu/tabbed-menu.component';
+import { SideMenuComponent } from './components/side-menu/side-menu.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DiaryComponent,
+    TabbedMenuComponent,
+    SideMenuComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/components/tabbed-menu/tabbed-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tabbed-menu',
+  templateUrl: './tabbed-menu.component.html',
+  styleUrls: ['./tabbed-menu.component.css']
+})
+export class TabbedMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
