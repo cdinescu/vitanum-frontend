a95098e16e6ba5e7a38ec46df60cda66581a4953
Cristina Dinescu <crina91@gmail.com>
2020-03-11 20:41:07 +0200

    When 'Diary'/'Ask the Oracle' are being clicked, then the side menu changes as well

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DiaryComponent } from './components/diary/diary.component';
 import { TabbedMenuComponent } from './components/tabbed-menu/tabbed-menu.component';
-import { SideMenuComponent } from './components/side-menu/side-menu.component';
 import { AskOracleComponent } from './components/ask-oracle/ask-oracle.component';
 import { AskOracleFormComponent } from './components/ask-oracle-form/ask-oracle-form.component';
 import { TopFoodsCountComponent } from './components/top-foods-count/top-foods-count.component';
@@ -30,7 +29,6 @@ const routes: Routes = [
     AppComponent,
     DiaryComponent,
     TabbedMenuComponent,
-    SideMenuComponent,
     AskOracleComponent,
     AskOracleFormComponent,
     TopFoodsCountComponent,
--- src/app/components/tabbed-menu/tabbed-menu.component.ts
+++ src/app/components/tabbed-menu/tabbed-menu.component.ts
@@ -1,15 +1,51 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  // ...
+} from '@angular/animations';
 
 @Component({
   selector: 'app-tabbed-menu',
   templateUrl: './tabbed-menu.component.html',
-  styleUrls: ['./tabbed-menu.component.css']
+  styleUrls: ['./tabbed-menu.component.css'],
+
+  animations: [
+    trigger('slideInOut', [
+      state('in', style({
+        transform: 'translate3d(0, 0, 0)'
+      })),
+      state('out', style({
+        transform: 'translate3d(100%, 0, 0)'
+      })),
+      transition('in => out', animate('400ms ease-in-out')),
+      transition('out => in', animate('400ms ease-in-out'))
+    ]),
+  ]
 })
 export class TabbedMenuComponent implements OnInit {
+  diaryMenuState:string = 'out';
+  askTheOracleMenuState:string = 'out';
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggleDiary() {
+    console.log("Toggled diary!");
+    // 1-line if statement that toggles the value:
+    this.askTheOracleMenuState = 'out';
+    this.diaryMenuState = this.diaryMenuState === 'out' ? 'in' : 'out';
+  }
+
+  toggleAskTheOracle() {
+    console.log("Toggled Ask the Oracle!");
+    this.diaryMenuState = 'out';
+    this.askTheOracleMenuState = this.askTheOracleMenuState === 'out' ? 'in' : 'out';
   }
 
 }
