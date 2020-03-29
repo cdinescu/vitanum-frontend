d7dc3028acc074e01c540e61e9f661948e83aaff
Cristina Dinescu <crina91@gmail.com>
2020-03-29 22:51:59 +0300

    Updated a little bit the fodd service to allows adding entries from 'top foods' component

--- src/app/services/food.service.ts
+++ src/app/services/food.service.ts
@@ -9,7 +9,7 @@ import { FoodNutrient } from '../common/food-nutrient';
 })
 export class FoodService {
 
-  private baseUrl = 'http://localhost:9090/foods';
+  private baseUrl = 'http://192.168.0.144:8082/foods';
 
   constructor(private httpClient: HttpClient) { }
 
@@ -21,16 +21,22 @@ export class FoodService {
 
   getNutrientReport(food: Food): Observable<FoodNutrient[]> {
     let foodId = food.fdcId;
+
+    if (foodId == null) {
+      return this.searchFoodAndGetNdbNumber(food);
+    }
+
     const requestUrl = `${this.baseUrl}/reports/${foodId}`;
-    console.log('Id: ' + food.fdcId);
+    console.log('Id: ' + food.fdcId + ' -----> ' + requestUrl);
 
     return this.httpClient.get<FoodNutrient[]>(requestUrl);
   }
 
-  searchFoodAndGetNdbNumber(food: Food): string {
-    let result: string;
+  searchFoodAndGetNdbNumber(food: Food): Observable<FoodNutrient[]> {
+    let result: Observable<FoodNutrient[]>;
     this.getSearchResult(food.description).subscribe(data => {
-      result = data[0].fdcId;
+      console.log('Finallt got: ' + data[0].description);
+      result = this.getNutrientReport(data[0]);
     });
 
     return result;
