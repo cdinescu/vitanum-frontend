a07b42cb8826345d592806c8bd39efc7a61e9bd0
Cristina Dinescu <crina91@gmail.com>
2020-04-04 16:26:42 +0300

    Created UT for the 'tabbed-menu' component

--- src/app/components/tabbed-menu/tabbed-menu.component.spec.ts
+++ src/app/components/tabbed-menu/tabbed-menu.component.spec.ts
@@ -8,9 +8,9 @@ describe('TabbedMenuComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TabbedMenuComponent ]
+      declarations: [TabbedMenuComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -19,7 +19,55 @@ describe('TabbedMenuComponent', () => {
     fixture.detectChanges();
   });
 
+  afterEach(() => {
+    component.diaryMenuState = 'out';
+    component.askTheOracleMenuState = 'out';
+  });
+
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have all tabs toggled by default', () => {
+    expect(component.diaryMenuState).toEqual('out');
+    expect(component.askTheOracleMenuState).toEqual('out');
+  });
+
+  it('toggle \'Diary\' when diary menu already out', () => {
+    component.toggleDiary();
+
+    expect(component.diaryMenuState).toEqual('in');
+    expect(component.askTheOracleMenuState).toEqual('out');
+  });
+
+  it('toggle \'Diary\' when diary menu in', () => {
+    // Arrange
+    component.diaryMenuState = 'in';
+
+    // Act
+    component.toggleDiary();
+
+    // Assert
+    expect(component.diaryMenuState).toEqual('out');
+    expect(component.askTheOracleMenuState).toEqual('out');
+  });
+
+  it('toggle \'AskOracle\' when ask oracle menu already out', () => {
+    component.toggleAskTheOracle();
+
+    expect(component.diaryMenuState).toEqual('out');
+    expect(component.askTheOracleMenuState).toEqual('in');
+  });
+
+  it('toggle \'AskOracle\' when ask oracle menu already in', () => {
+    // Arrange
+    component.askTheOracleMenuState = 'in';
+
+    // Act
+    component.toggleAskTheOracle();
+
+    // Assert
+    expect(component.diaryMenuState).toEqual('out');
+    expect(component.askTheOracleMenuState).toEqual('out');
   });
 });
