29e0b7720fa29da1c3ccc150e97c6ec0b6880e47
Cristina Dinescu <crina91@gmail.com>
2020-04-05 16:07:38 +0300

    New integration tests

--- src/app/components/food-logger-entry/food-logger-entry.component.spec.ts
+++ src/app/components/food-logger-entry/food-logger-entry.component.spec.ts
@@ -6,8 +6,10 @@ import { FormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
 import { MaterialModule } from 'src/app/material.module';
 import { MatDialogModule } from '@angular/material/dialog';
-import { Observable } from 'rxjs';
+import { of } from 'rxjs';
 import { Food } from 'src/app/common/food';
+import { DiaryServiceService } from 'src/app/services/diary-service.service';
+import { DiaryEntry } from 'src/app/common/diary-entry';
 
 describe('FoodLoggerEntryComponent', () => {
   let component: FoodLoggerEntryComponent;
@@ -24,44 +26,55 @@ describe('FoodLoggerEntryComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(FoodLoggerEntryComponent);
     component = fixture.componentInstance;
-
-    const testFood = new Food();
-    testFood.description = 'test food';
-    testFood.measure = 'g';
-    testFood.quantity = 100;
-
-    component.selectedFood = testFood;
-
-    component.foodAboutToBeLogged = new Observable<Food>();
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-
-  it('should have available measurements', () => {
-    expect(component.availableFoodMesurements).toEqual(['g', 'mg', 'l', 'ml']);
-  });
-
-  it('should have a selected measure', () => {
-    expect(component.selectedMesurement).toEqual('g');
-  });
-
-  it('should reset entry', () => {
+  it('should initialize selected food in component init', () => {
     // Arrange
-    component.servingsCount = 125;
+    const testFood = createFood();
+    component.foodAboutToBeLogged = of(testFood);
 
     // Act
-    component.resetFoodSelection();
+    fixture.detectChanges();
 
     // Assert
-    expect(component.selectedFood).toEqual(null);
-    expect(component.servingsCount).toEqual(1);
+    expect(component.servingsCount).toBe(1);
+    expect(component.selectedFood).toEqual(testFood);
   });
 
-  it('should be able to add entry', () => {
-    expect(component.addEntryToDiary()).toBe();
+  it('should trigger an update model in the service', () => {
+    // Arrange
+    const testFood = createFood();
+    component.foodAboutToBeLogged = of(testFood);
+    fixture.detectChanges();
+
+    const diaryService: DiaryServiceService = TestBed.get(DiaryServiceService);
+    spyOn(diaryService, 'postDiaryEntry').and.returnValue(of(createDiaryEntry()));
+    const spy = spyOn(diaryService, 'nextUpdateDiaryEntryModelQuery');
+
+    // Act
+    component.addEntryToDiary();
+
+    // Assert
+    expect(spy).toHaveBeenCalledTimes(1);
   });
 
 });
+
+function createFood() {
+  const testFood = new Food();
+  testFood.description = 'test food';
+  testFood.measure = 'g';
+  testFood.quantity = 100;
+  return testFood;
+}
+
+function createDiaryEntry() {
+  const diaryEntry = new DiaryEntry();
+  diaryEntry.description = 'new diary entry';
+  diaryEntry.amount = 100;
+  diaryEntry.calories = 150;
+  diaryEntry.unit = 'g';
+
+  return diaryEntry;
+}
+
---
+++ src/app/components/food-logger-entry/food-logger-entry.unit.component.spec.ts
@@ -0,0 +1,64 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FoodLoggerEntryComponent } from './food-logger-entry.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MaterialModule } from 'src/app/material.module';
+import { MatDialogModule } from '@angular/material/dialog';
+import { Observable } from 'rxjs';
+import { Food } from 'src/app/common/food';
+import { DiaryServiceService } from 'src/app/services/diary-service.service';
+
+describe('FoodLoggerEntryComponent', () => {
+  let component: FoodLoggerEntryComponent;
+  let fixture: ComponentFixture<FoodLoggerEntryComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule, FormsModule, MatIconModule, MaterialModule, MatDialogModule],
+      declarations: [FoodLoggerEntryComponent]
+    })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FoodLoggerEntryComponent);
+    component = fixture.componentInstance;
+
+    const testFood = new Food();
+    testFood.description = 'test food';
+    testFood.measure = 'g';
+    testFood.quantity = 100;
+
+    component.selectedFood = testFood;
+
+    component.foodAboutToBeLogged = new Observable<Food>();
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should have available measurements', () => {
+    expect(component.availableFoodMesurements).toEqual(['g', 'mg', 'l', 'ml']);
+  });
+
+  it('should have a selected measure', () => {
+    expect(component.selectedMesurement).toEqual('g');
+  });
+
+  it('should reset entry', () => {
+    // Arrange
+    component.servingsCount = 125;
+
+    // Act
+    component.resetFoodSelection();
+
+    // Assert
+    expect(component.selectedFood).toEqual(null);
+    expect(component.servingsCount).toEqual(1);
+  });
+
+});
